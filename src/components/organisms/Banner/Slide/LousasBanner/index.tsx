63faa7b97830eb3610ed472c50e47e03bec405f9
Ryan Ferreira Cardoso <ryan.cardoso@saudeid.com.br>
2023-03-30 16:38:39 -0300

    improving first banner design

--- src/components/organisms/Banner/Slide/LousasBanner/index.tsx
+++ src/components/organisms/Banner/Slide/LousasBanner/index.tsx
@@ -1,16 +1,39 @@
 // Libs
 import React from "react";
+import Image from "next/image";
 
 // Styles
 import * as S from "./styles";
 
 export const LousasBanner = () => {
+  const dataBenefits = [
+    "Garantia de 10 anos",
+    "Não mancha e não risca",
+    "Muitas opções de cores",
+    "Feita sob medida",
+  ];
+
   return (
     <S.Container>
-      <S.Description data-aos="fade-up">
-        <S.Type>Lousas de vidro</S.Type>
-        <S.Title>Lousas personalizadas sob medida</S.Title>
-      </S.Description>
+      <S.Wrapper>
+        <S.Description data-aos="fade-up">
+          <S.Title>Lousa em vidro</S.Title>
+          <S.List>
+            {dataBenefits.map((item, index) => (
+              <li key={index}>
+                <Image
+                  src="/images/icon_verified.svg"
+                  alt="ícone de verificado"
+                  width="20"
+                  height="20"
+                />
+                <p>{item}</p>
+              </li>
+            ))}
+          </S.List>
+        </S.Description>
+        <S.Figure />
+      </S.Wrapper>
     </S.Container>
   );
 };
--- src/components/organisms/Banner/Slide/LousasBanner/styles.ts
+++ src/components/organisms/Banner/Slide/LousasBanner/styles.ts
@@ -2,6 +2,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
 
@@ -10,53 +11,91 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
 
+  background: linear-gradient(180deg, #3A4B73 0%, #101D3F 100%);
+`;
+
+export const Wrapper = styled.div`
+  max-width: 1440px;
+  width: 95%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+
+  @media (max-width: 768px) {
+    width: 85%;
+    justify-content: flex-start;
+  }
+`
+
+export const Description = styled.div`
+  width: 40%;
+
+  @media (max-width: 768px) {
+    min-width: 190px;
+  }
+`;
+
+export const Title = styled.h1`
+  color: #1EFF5D;
+  font-size: 48px;
+  line-height: 64px;
+
+  @media (max-width: 768px) {
+    font-size: 24px;
+    line-height: 36px;
+  }
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+
+  gap: 8px;
+  margin-top: 12px;
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    p {
+      color: #FFFFFF;
+    }
+
+    @media (max-width: 768px) { 
+      p {
+        font-size: 14px;
+        line-height: 20px;
+      }
+    }
+  }
+`;
+
+export const Figure = styled.figure`
+  max-width: 653px;
+  width: 60%;
+  height: 329px;
+
+  background-color: #D9D9D9;
   background-image: url("/images/lousas_banner.png");
   background-repeat: no-repeat;
   background-size: cover;
 
-  @media (max-width: 600px) {
+  border-radius: 181.887px 0px 0px 181.887px;
+
+  @media (max-width: 768px) { 
+    position: absolute;
+    width: 42%;
+    height: 230px;
+    right: 0;
+  
+    background-position: 70% 0;
+    border-radius: 74.7405px 0px 0px 74.7405px;
+  }
+
+  @media (max-width: 500px) {
     background-image: url("/images/lousas_banner_mobile.png");
   }
-`;
-
-export const Description = styled.div`
-  position: relative;
-  width: 95%;
-  max-width: 1440px;
-
-  color: #ffffff;
-
-  &::after {
-    content: "";
-
-    position: absolute;
-    left: 0;
-    top: 0;
-
-    width: 269px;
-    height: 136px;
-    z-index: -1;
-
-    background: linear-gradient(
-      180deg,
-      #4a8eff 2.08%,
-      rgba(50, 110, 219, 0.37) 100%
-    );
-    background-blend-mode: darken;
-
-    filter: blur(16px);
-  }
-
-  @media (max-width: 768px) {
-    width: 85%;
-  }
-`;
-
-export const Type = styled.p`
-  font-size: 20px;
-`;
-
-export const Title = styled.h1`
-  width: 248px;
-  font-size: 24px;
-`;
+`
